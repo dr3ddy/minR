4ff06932d74b9c42c08d350a13f346fc2a60806b
dr3ddy <eepr3ddy@gmail.com>
2015-09-29 08:36:44 +0530

    Initial Commit
    First Commit

---
+++ js/minr.js
@@ -0,0 +1,53 @@
+$(document).ready(function() {
+	
+	$('body').scrollspy({
+			target: '#navbar',
+			offset: 50
+	});
+	
+	$('a[href*=#]').bind("click", function(e){
+		var target = $(this);
+		$('html, body').stop().animate({
+			scrollTop: $(target.attr('href')).offset().top
+		}, 1000);
+		e.preventDefault();
+	});
+});
+
+$(window).scroll(function() {			
+	if($(this).scrollTop() > 100) { 
+		$('.navbar').addClass('navbar-white'); 
+	} else {
+		$('.navbar').removeClass('navbar-white'); 
+	}
+});
+	  
+$('.scrollToTop').click(function () {
+	$('html, body').animate({
+		scrollTop: 0
+	}, 400);
+	return false;
+});
+
+$(window).scroll(function () {
+    if ($(this)
+            .scrollTop() > 100) {
+        $('.scrollToTop')
+                .fadeIn();
+    } else {
+        $('.scrollToTop')
+                .fadeOut();
+    }
+});
+
+$('#left').waypoint(function() {
+	$('#left').css("opacity", "1");
+	$(this).toggleClass('animate slideInLeft');
+},
+{ offset: '90%', triggerOnce: true });
+
+$('#right').waypoint(function() {
+	$('#right').css("opacity", "1");
+	$(this).toggleClass('animate slideInRight');
+},
+{ offset: '90%', triggerOnce: true });
